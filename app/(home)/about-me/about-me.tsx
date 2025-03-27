b98b117c7bf8ef154d3356d681442680a90abea1
Cho-SeongJu <tjdwn9753@gmail.com>
2025-03-27 21:13:02 +0900

    💄  Style: change about me text

--- app/(home)/about-me/about-me.tsx
+++ app/(home)/about-me/about-me.tsx
@@ -25,12 +25,12 @@ export default function AboutMe({ targetRef, setStep }: IAboutMeProps) {
           <div className="mt-[24px] text-[25px] leading-150 flex flex-col">
             <div className="flex overflow-hidden mx-auto">
               <span className="fade-up-animation opacity-0">
-                비즈니스 관점을 고려하며 효율적인 코드 구조를 고민하고,
+                주도적이고 꾸준히 성장하는 개발자가 되기 위해 노력하는
               </span>
             </div>
             <div className="flex overflow-hidden mx-auto">
               <span className="fade-up-animation opacity-0">
-                중복을 줄여 유지보수성을 높이는 프론트엔드 개발자 조성주입니다.
+                신입 프론트엔드 개발자 조성주입니다.
               </span>
             </div>
           </div>
