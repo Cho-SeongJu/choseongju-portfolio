65fe8e1d4c39ef19a595874ca03d01849fd4ab36
Cho-SeongJu <tjdwn9753@gmail.com>
2025-04-08 22:28:09 +0900

    💄 Style: add responsive css

--- app/(home)/header/logo.tsx
+++ app/(home)/header/logo.tsx
@@ -8,17 +8,17 @@ export default function Logo({ step }: ILogo) {
   return (
     <div
       className={`${
-        step === "intro" ? "w-full pb-[60px]" : "pb-0 w-[170px]"
-      } absolute left-[50%] translate-x-[-50%] top-0 font-title mx-auto text-center text-white-001 intro-animation transition-all`}
+        step === "intro"
+          ? "w-full xl:translate-y-[calc(100vh-100%-160px)] lg:translate-y-[calc(100vh-100%-180px)] xs:translate-y-[calc(100vh-100%-200px)]"
+          : "pb-0 w-[170px] translate-y-[16px]"
+      } absolute top-0 left-[50%] translate-x-[-50%] font-title mx-auto text-center text-white-001 intro-animation transition-all`}
     >
-      <h1
-        className={`${
-          step === "intro" ? "translate-y-[75%]" : "mx-auto translate-y-[16px]"
-        } intro-animation`}
-      >
+      <h1 className={`${step !== "intro" && "mx-auto"} intro-animation`}>
         <span
           className={`${
-            step === "intro" ? "text-[110px]" : "text-[14.54px]"
+            step === "intro"
+              ? "xl:text-[110px] lg:text-[80px] md:text-[60px] sm:text-[45px] xs:text-[26px]"
+              : "sm:text-[14.54px] xs:text-[10.72px]"
           } inline-block align-middle tracking-1 intro-animation`}
         >
           Front-End Developer
@@ -26,8 +26,8 @@ export default function Logo({ step }: ILogo) {
         <span
           className={`${
             step === "intro"
-              ? "text-stroke-intro mt-[30px] text-[200px]"
-              : "text-stroke text-[22.37px]"
+              ? "text-stroke-intro mt-[30px] xl:text-[200px] lg:text-[150px] md:text-[110px] sm:text-[70px] xs:text-[40px]"
+              : "text-stroke sm:text-[22.37px] xs:text-[16.5]"
           } inline-block align-middle tracking-1 intro-animation`}
         >
           Cho Seong Ju
