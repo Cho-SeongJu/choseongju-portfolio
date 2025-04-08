0928ba7e3c2d17e171ec96ecee2f006cb8bf8697
Cho-SeongJu <tjdwn9753@gmail.com>
2025-04-08 23:18:36 +0900

    💄 Style: margin top responsive

--- app/(home)/header/logo.tsx
+++ app/(home)/header/logo.tsx
@@ -18,7 +18,7 @@ export default function Logo({ step }: ILogo) {
         <span
           className={`${
             step === "intro"
-              ? "xl:text-[110px] lg:text-[80px] md:text-[60px] sm:text-[45px] xs:text-[26px]"
+              ? "xl:text-[110px] lg:text-[80px] md:text-[60px] sm:text-[40px] xs:text-[26px]"
               : "sm:text-[14.54px] xs:text-[10.72px]"
           } inline-block align-middle tracking-1 intro-animation`}
         >
@@ -27,7 +27,7 @@ export default function Logo({ step }: ILogo) {
         <span
           className={`${
             step === "intro"
-              ? "text-stroke-intro mt-[30px] xl:text-[200px] lg:text-[150px] md:text-[110px] sm:text-[70px] xs:text-[40px]"
+              ? "text-stroke-intro sm:mt-[30px] xs:mt-[15px] xl:text-[200px] lg:text-[150px] md:text-[110px] sm:text-[60px] xs:text-[40px]"
               : "text-stroke sm:text-[22.37px] xs:text-[16.5]"
           } inline-block align-middle tracking-1 intro-animation`}
         >
