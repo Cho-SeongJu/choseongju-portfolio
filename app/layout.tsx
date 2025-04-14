2f192f8d00207f0bd917778fba46438152f7cb74
Cho-SeongJu <tjdwn9753@gmail.com>
2025-04-14 23:01:49 +0900

    💄 Style: change title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from "next";
 import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Seong Ju Portfolio",
+  description: "조성주의 포트폴리오 사이트 입니다.",
+};
 
 export default function RootLayout({
   children,
@@ -7,7 +13,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <title>조성주의 포트폴리오</title>
       <body className="bg-black-001 font-default">{children}</body>
     </html>
   );
